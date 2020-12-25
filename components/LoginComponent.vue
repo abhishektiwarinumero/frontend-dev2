296c467014280e037a97c7dd2b5bf7e531e3d4e5
Salim Djerbouh <caddydz4@gmail.com>
2020-12-25 10:00:00 +0100

    Formatting
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- components/LoginComponent.vue
+++ components/LoginComponent.vue
@@ -1,47 +1,47 @@
 <template>
-	<div>
-		<v-btn
-			class="ma-2"
-			outlined
-			color="white"
-			:href="`https://${process.env.HOST_URL}/dashboard`"
-			v-if="this.$auth.loggedIn"
-			>Members Area</v-btn
-		>
-		<v-dialog max-width="800px" v-model="dialog" v-else>
-			<template v-slot:activator="{ on }">
-				<v-btn class="ma-2" outlined color="white" v-on="on">
-					Members Area
-				</v-btn>
-			</template>
-			<v-card class="px-5" color="#303030">
-				<v-container>
-					<v-row>
-						<v-col cols="7">
-							<LoginForm @close="dialog = false"></LoginForm>
-						</v-col>
-						<v-col cols="5">
-							<SocialLogin></SocialLogin>
-						</v-col>
-					</v-row>
-				</v-container>
-			</v-card>
-		</v-dialog>
-	</div>
+  <div>
+    <v-btn
+      class="ma-2"
+      outlined
+      color="white"
+      :href="`https://${process.env.HOST_URL}/dashboard`"
+      v-if="this.$auth.loggedIn"
+      >Members Area</v-btn
+    >
+    <v-dialog max-width="800px" v-model="dialog" v-else>
+      <template v-slot:activator="{ on }">
+        <v-btn class="ma-2" outlined color="white" v-on="on">
+          Members Area
+        </v-btn>
+      </template>
+      <v-card class="px-5" color="#303030">
+        <v-container>
+          <v-row>
+            <v-col cols="7">
+              <LoginForm @close="dialog = false"></LoginForm>
+            </v-col>
+            <v-col cols="5">
+              <SocialLogin></SocialLogin>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			dialog: false,
-		};
-	},
+  data() {
+    return {
+      dialog: false,
+    };
+  },
 };
 </script>
 
 <style scoped>
 .v-btn:hover:before {
-	content: none;
+  content: none;
 }
 </style>
