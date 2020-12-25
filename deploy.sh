7a5aeeb07a2ba63fa4912ec31d48506693de72af
Salim Djerbouh <caddydz4@gmail.com>
2020-12-25 09:08:23 +0100

    Build overriding nuxt lock
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ cd /home/forge/staging.kingboosting.com
 git checkout .
 git pull origin develop
 npm install --production
-npm run build
+npm run build --no-lock
 pm2 start npm --name "staging.kingboosting.com" --watch -- start
