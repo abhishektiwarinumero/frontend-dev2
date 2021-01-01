602a102ef763fec07b987e1405cffb20f36f45e1
Salim Djerbouh <caddydz4@gmail.com>
2021-01-01 07:34:38 +0100

    Extracted SEO to a module
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 import webpack from "webpack";
+import seo from "./seo";
 
 export default {
 	vue: {
@@ -9,44 +10,8 @@ export default {
 	// Global page headers (https://go.nuxtjs.dev/config-head)
 	/*
 	 ** Head elements
-	 ** Add Roboto font and Material Icons
 	 */
-	head: {
-		title: "Most Affordable Premium LOL Boosting & Valorant ELO Boost",
-		titleTemplate: "%s | King Boosting",
-		htmlAttrs: {
-			lang: 'en',
-		},
-		meta: [{
-			charset: "utf-8"
-		},
-		{
-			name: "viewport",
-			content: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-		},
-		{
-			hid: "description",
-			name: "description",
-			content: "Premium LoL & Valorant Boosting, Coaching, and Account Selling, all at an Affordable Price! Safe, fast, effortless delivery to ALL Regions! Don't wait, RANK UP NOW!"
-		},
-		{
-			name: "keywords",
-			content: "elo boost, lol elo boost, lol boost, lol elo boosting, elo boost lol, cheap elo boosting, elo boosting"
-		}
-		],
-		link: [{
-			rel: "icon",
-			type: "image/x-icon",
-			href: "/favicon.ico"
-		},
-		],
-		script: [{
-			src: '//code.tidio.co/bytmjjusa3gkjkwwkmqjbag7x3lzanqk.js'
-		}],
-		noscript: [{
-			innerHTML: 'This website requires JavaScript.'
-		}]
-	},
+	head: seo,
 
 	// Global CSS (https://go.nuxtjs.dev/config-css)
 	css: [
---
+++ seo.js
@@ -0,0 +1,36 @@
+export default {
+	title: "Most Affordable Premium LOL Boosting & Valorant ELO Boost",
+	titleTemplate: "%s | King Boosting",
+	htmlAttrs: {
+		lang: 'en',
+	},
+	meta: [{
+		charset: "utf-8"
+	},
+	{
+		name: "viewport",
+		content: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+	},
+	{
+		hid: "description",
+		name: "description",
+		content: "Premium LoL & Valorant Boosting, Coaching, and Account Selling, all at an Affordable Price! Safe, fast, effortless delivery to ALL Regions! Don't wait, RANK UP NOW!"
+	},
+	{
+		name: "keywords",
+		content: "elo boost, lol elo boost, lol boost, lol elo boosting, elo boost lol, cheap elo boosting, elo boosting"
+	}
+	],
+	link: [{
+		rel: "icon",
+		type: "image/x-icon",
+		href: "/favicon.ico"
+	},
+	],
+	script: [{
+		src: '//code.tidio.co/bytmjjusa3gkjkwwkmqjbag7x3lzanqk.js'
+	}],
+	noscript: [{
+		innerHTML: 'This website requires JavaScript.'
+	}]
+};
