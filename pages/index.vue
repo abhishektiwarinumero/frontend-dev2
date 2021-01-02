224d79b95f4ab3d6ee6f0ff3344553714d4f7670
Salim Djerbouh <caddydz4@gmail.com>
2021-01-02 15:49:45 +0100

    Services section design
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <v-row align="center" justify="center" class="mt-16">
         <img src="/img/logo/logo.png" alt="Logo" width="55%" class="mt-16" />
       </v-row>
-      <v-row align="center" justify="center" class="mt-16 mb-16">
+      <v-row align="end" justify="center" class="mt-16 mb-16">
         <v-btn-toggle rounded v-model="undefined">
           <v-btn x-large class="text-h4" color="black" style="padding: 40px"
             >Get Boosted</v-btn
@@ -17,8 +17,45 @@
           >
         </v-btn-toggle>
       </v-row>
+      <v-row justify="center" align="center" class="mt-16 mb-16">
+        <p class="text-h2 mt-16">Our Services</p>
+      </v-row>
+      <!-- 5 games here -->
+      <v-row
+        justify="center"
+        align="center"
+        class="mt-16 mb-16"
+        dense
+        no-gutters
+      >
+        <v-col class="lg5-custom">
+          <v-img src="/img/games/cards/lol_boost.png"></v-img>
+        </v-col>
+        <v-col class="lg5-custom">
+          <v-img src="/img/games/cards/valorant_boost.png"></v-img>
+        </v-col>
+        <v-col class="lg5-custom">
+          <v-img src="/img/games/cards/tft_boost.png"></v-img>
+        </v-col>
+        <v-col class="lg5-custom">
+          <v-img src="/img/games/cards/wildrift_boost.png"></v-img>
+        </v-col>
+        <v-col class="lg5-custom">
+          <v-img src="/img/games/cards/runeterra_boost.png"></v-img>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center" class="mt-16 mb-16">
+        <v-col sm="4" md="4" lg="4">
+          <v-img src="/img/games/cards/unranked_smurf.png"></v-img>
+        </v-col>
+        <v-col sm="4" md="4" lg="4">
+          <v-img src="/img/games/cards/coaching.png"></v-img>
+        </v-col>
+        <v-col sm="4" md="4" lg="4">
+          <v-img src="/img/games/cards/account_market.png"></v-img>
+        </v-col>
+      </v-row>
     </v-container>
-    <services-circle />
     <features />
     <customer-voice />
     <how-to-buy />
