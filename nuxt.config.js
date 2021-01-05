9005efe3e8feb8658cbab54a39ae6d8049884fbb
Salim Djerbouh <caddydz4@gmail.com>
2021-01-05 16:07:52 +0100

    Use host url as axios fallback
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
 	],
 
 	axios: {
-		baseURL: 'http://localhost:8000', // Used as fallback if no runtime config is provided
+		baseURL: process.env.HOST_URL, // Used as fallback if no runtime config is provided
 		proxy: true,
 		credentials: true
 	},
