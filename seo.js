28b3182eb1e53ef1a5d24a6ccc34a33f0a2cf4b8
Salim Djerbouh <caddydz4@gmail.com>
2021-01-08 13:20:22 +0100

    Override font
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- seo.js
+++ seo.js
@@ -5,27 +5,31 @@ export default {
 		lang: 'en',
 	},
 	meta: [{
-		charset: "utf-8"
-	},
-	{
-		name: "viewport",
-		content: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-	},
-	{
-		hid: "description",
-		name: "description",
-		content: "Premium LoL & Valorant Boosting, Coaching, and Account Selling, all at an Affordable Price! Safe, fast, effortless delivery to ALL Regions! Don't wait, RANK UP NOW!"
-	},
-	{
-		name: "keywords",
-		content: "elo boost, lol elo boost, lol boost, lol elo boosting, elo boost lol, cheap elo boosting, elo boosting"
-	}
+			charset: "utf-8"
+		},
+		{
+			name: "viewport",
+			content: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+		},
+		{
+			hid: "description",
+			name: "description",
+			content: "Premium LoL & Valorant Boosting, Coaching, and Account Selling, all at an Affordable Price! Safe, fast, effortless delivery to ALL Regions! Don't wait, RANK UP NOW!"
+		},
+		{
+			name: "keywords",
+			content: "elo boost, lol elo boost, lol boost, lol elo boosting, elo boost lol, cheap elo boosting, elo boosting"
+		}
 	],
 	link: [{
-		rel: "icon",
-		type: "image/x-icon",
-		href: "/favicon.ico"
-	},
+			rel: "icon",
+			type: "image/x-icon",
+			href: "/favicon.ico"
+		},
+		{
+			rel: 'stylesheet',
+			href: '/fonts/evogria/style.css'
+		},
 	],
 	script: [{
 		// src: '//code.tidio.co/bytmjjusa3gkjkwwkmqjbag7x3lzanqk.js'
