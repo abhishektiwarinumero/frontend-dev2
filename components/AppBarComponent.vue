af43a9eebc6f5c810167079ce24794337cbbcea3
Salim Djerbouh <caddydz4@gmail.com>
2021-01-08 21:39:57 +0100

    Formatting
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- components/AppBarComponent.vue
+++ components/AppBarComponent.vue
@@ -1,49 +1,47 @@
 <template>
-  <v-app-bar app flat color="#1e1e1e" prominent height="88">
-    <div id="particles-js"></div>
-    <nuxt-link to="/">
-      <img src="/img/logo/nav.png" alt="Logo" height="88" />
-    </nuxt-link>
+	<v-app-bar app flat color="#1e1e1e" prominent height="88">
+		<div id="particles-js"></div>
+		<nuxt-link to="/">
+			<img src="/img/logo/nav.png" alt="Logo" height="88" />
+		</nuxt-link>
 
-    <v-tabs centered color="primary" optional slider-size="4">
-      <v-tab v-for="link in links" :key="link.title" nuxt :to="link.url">
-        {{ link.title }}
-      </v-tab>
-    </v-tabs>
-    <members-area></members-area>
-  </v-app-bar>
+		<v-tabs centered color="primary" optional slider-size="4">
+			<v-tab v-for="link in links" :key="link.title" nuxt :to="link.url">{{ link.title }}</v-tab>
+		</v-tabs>
+		<members-area></members-area>
+	</v-app-bar>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    links: [
-      { title: "Elo Boosting", url: "division-boosting" },
-      { title: "Coaching", url: "coaching" },
-      { title: "Account Market", url: "lol-accounts" },
-      { title: "More", url: "" },
-    ],
-  }),
+	data: () => ({
+		links: [
+			{ title: "Elo Boosting", url: "division-boosting" },
+			{ title: "Coaching", url: "coaching" },
+			{ title: "Account Market", url: "lol-accounts" },
+			{ title: "More", url: "" },
+		],
+	}),
 };
 </script>
 
 <style scoped>
 .header_logo {
-  padding-top: 30px;
+	padding-top: 30px;
 }
 #particles-js canvas {
-  display: block;
-  transform: scale(1);
-  opacity: 1;
-  transition: opacity 0.8s ease, transform 1.4s ease;
+	display: block;
+	transform: scale(1);
+	opacity: 1;
+	transition: opacity 0.8s ease, transform 1.4s ease;
 }
 
 #particles-js {
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  z-index: -10;
-  top: 0;
-  left: 0;
+	width: 100%;
+	height: 100%;
+	position: fixed;
+	z-index: -10;
+	top: 0;
+	left: 0;
 }
 </style>
