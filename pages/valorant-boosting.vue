750bf365ce94ec13b64cbd521ad39d5981889436
Salim Djerbouh <caddydz4@gmail.com>
2021-01-12 23:41:39 +0100

    Valorant boosting
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/valorant-boosting.vue
+++ pages/valorant-boosting.vue
@@ -1,14 +1,125 @@
 <template>
-  <div>
-    <p class="text-overline">Valorant Boost</p>
-    <p class="font-weight-bold text-h3">Valorant Boosting</p>
-    <br />Our premium Valorant Boost service offers an efficient &amp; secure
-    option to reach any specific rank or goal.
-  </div>
+	<v-layout column full fill-height class="page">
+		<v-container>
+			<v-row class="flex-center">
+				<img src="/img/logo/logo.png" width="700" />
+			</v-row>
+			<games-slider />
+			<v-row class="flex-center mb-4">
+				<h1>Valorant Boosting</h1>
+			</v-row>
+			<game-services :services="gameServices" icons />
+			<v-row class="flex-center">
+				<v-row class="order-form">
+					<v-form ref="order">
+						<v-row>
+							<v-col cols="12" sm="6" md="7">
+								<select-current-league />
+								<select-desired-league />
+							</v-col>
+							<v-col cols="6" md="5">
+								<checkout :options="options" />
+								<boost-me />
+							</v-col>
+						</v-row>
+					</v-form>
+				</v-row>
+			</v-row>
+		</v-container>
+		<why-us />
+		<customers-voice />
+	</v-layout>
 </template>
 
 <script>
+import games from "~/assets/js/games";
+import services from "~/assets/js/services";
 export default {
-  layout: "game",
+	transition: "slide-bottom",
+	data: () => ({
+		games: games,
+		// We need all services in order to grab the first one of each game
+		// Then we use that to define the link of the first service in a game
+		services: services,
+		options: [
+			{
+				checked: false,
+				icon: "mdi-account-remove",
+				title: "Appear offline on chat",
+				percentage: 0,
+				tip:
+					"This option will make your account offline in the League Client chat, therefore your friends won't see when the booster is playing on your account",
+			},
+			{
+				checked: false,
+				icon: "mdi-account-group",
+				title: "Specific champions",
+				percentage: 0,
+				tip:
+					"You may determine what role(s) your booster will queue up for while playing on your account. For additional costs, you may also determine what champions he should play.",
+			},
+			{
+				checked: false,
+				icon: "mdi-flash",
+				title: "Priority order (2x speed) at",
+				percentage: 20,
+				tip:
+					"This option ensures that your order will be treated with a higher priority, thus resulting in a faster completion.",
+			},
+			{
+				checked: false,
+				icon: "mdi-video",
+				title: "With Streaming at",
+				percentage: 15,
+				tip:
+					"Your assigned booster will record/live stream all the games, depending on your requirements.",
+			},
+			{
+				checked: false,
+				icon: "mdi-plus",
+				title: "+1 Win in desired rank",
+				// This needs to be dynamic, based on the desired rank selected
+				percentage: 0,
+				tip:
+					"By selecting this option, you'll receive an additional net win once your desired rank was reached.",
+			},
+		],
+	}),
+	computed: {
+		game() {
+			// How do we get the game
+			// Router name is a service slug
+			// Get that service game
+			let service = _.find(this.services, ["slug", this.$route.name]);
+			return _.find(this.games, ["name", service.game]);
+		},
+		gameServices() {
+			return _.filter(this.services, ["game", this.game.name]);
+		},
+	},
+	mounted() {
+		// So price doesn't get multiplied, because there are no wins in division boosting
+		this.$store.commit("wins/changeNumberOfWins", 1);
+	},
 };
 </script>
+
+<style scoped>
+.page {
+	background: url(/img/backgrounds/order.png);
+	background-position: top;
+	background-size: cover;
+	background-repeat: no-repeat;
+}
+
+.flex-center {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.order-form {
+	padding: 10px;
+	background: #eeea;
+}
+</style>
