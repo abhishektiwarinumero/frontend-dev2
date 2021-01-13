ea64f21b980897b5c86837bb8e582badfd93a872
Salim Djerbouh <caddydz4@gmail.com>
2021-01-13 19:57:35 +0100

    Moved hero from component to page
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
 	<v-layout column fill-height>
-		<kingboosting />
+		<section>
+			<v-img src="/img/indexAssets/kingBoosting.png" class="bg" />
+			<v-container>
+				<v-row align="center" justify="center" class="logo">
+					<img src="/img/logo/logo.png" alt="Logo" width="55%" class="mt-15" />
+				</v-row>
+				<v-row align="end" justify="center" class="mt-7 mb-14">
+					<v-btn-toggle rounded v-model="undefined" background-color="transparent">
+						<v-btn class="hero-btn">Get Boosted</v-btn>
+						<v-btn class="hero-btn">Get Coached</v-btn>
+						<v-btn class="hero-btn">Buy an account</v-btn>
+					</v-btn-toggle>
+				</v-row>
+			</v-container>
+		</section>
 		<services />
 		<features />
 		<why-us />
@@ -8,3 +22,52 @@
 		<customers-voice />
 	</v-layout>
 </template>
+
+
+<style scoped>
+section {
+	position: relative;
+	width: 100%;
+	box-sizing: border-box;
+	padding-bottom: 80px;
+}
+.bg {
+	position: absolute;
+	width: 100%;
+	filter: contrast(70%);
+	z-index: 1;
+}
+.container {
+	position: relative;
+	z-index: 3;
+}
+.logo {
+	margin-top: 160px;
+}
+.hero-btn {
+	background: black;
+	box-sizing: border-box;
+	padding: 0 48px !important;
+	font-size: 14px;
+	margin: 0 2px;
+}
+.hero-btn:hover {
+	background-color: #472783 !important;
+}
+
+.round-left {
+	border-radius: 80px 0 0 80px;
+}
+.round-right {
+	border-radius: 0 80px 80px 0;
+}
+.mtr-12 {
+	margin-top: 192px;
+}
+@media only screen and (max-width: 1200px) {
+	.bg {
+		width: inherit;
+		height: 100%;
+	}
+}
+</style>
