e8299ef17a7742939ff350264260d912d6b09c50
Salim Djerbouh <caddydz4@gmail.com>
2021-01-13 14:38:16 +0100

    Formatting
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/lor-boosting.vue
+++ pages/lor-boosting.vue
@@ -1,16 +1,14 @@
 <template>
-  <div>
-    <p class="text-overline">LOR Boosting</p>
-    <p class="font-weight-bold text-h3">
-      League Of Runeterra Boost - LoR Boosting
-    </p>
-    <br />Reach the highest ranks in Runeterra by hiring challenger pros to
-    boost your tier in Runeterra.
-  </div>
+	<div>
+		<p class="text-overline">LOR Boosting</p>
+		<p class="font-weight-bold text-h3">League Of Runeterra Boost - LoR Boosting</p>
+		<br />Reach the highest ranks in Runeterra by hiring challenger pros to
+		boost your tier in Runeterra.
+	</div>
 </template>
 
 <script>
 export default {
-  layout: "game",
+	layout: "game",
 };
 </script>
