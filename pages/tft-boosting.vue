a6dd7713e530cf8441c06da75a2238a841fa4df1
Salim Djerbouh <caddydz4@gmail.com>
2021-01-13 14:40:14 +0100

    Formatting
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/tft-boosting.vue
+++ pages/tft-boosting.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <p class="text-overline">TFT Boosting</p>
-    <p class="font-weight-bold text-h3">Teamfight Tactics Ranked Boost</p>
-    <br />TFT Boost service allows you to boost your ranking in TFT with the
-    help of our challenger employees.
-  </div>
+	<div>
+		<p class="text-overline">TFT Boosting</p>
+		<p class="font-weight-bold text-h3">Teamfight Tactics Ranked Boost</p>
+		<br />TFT Boost service allows you to boost your ranking in TFT with the
+		help of our challenger employees.
+	</div>
 </template>
 
 <script>
 export default {
-  layout: "game",
+	layout: "game",
 };
 </script>
--- pages/wild-rift-boosting.vue
+++ pages/wild-rift-boosting.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <p class="text-overline">Wild Rift Boosting</p>
-    <p class="font-weight-bold text-h3">LoL Wild Rift Boost</p>
-    <br />Our premium LoL Wild Rift Boost service offers an easy and secure
-    solution to reach any rank or goal.
-  </div>
+	<div>
+		<p class="text-overline">Wild Rift Boosting</p>
+		<p class="font-weight-bold text-h3">LoL Wild Rift Boost</p>
+		<br />Our premium LoL Wild Rift Boost service offers an easy and secure
+		solution to reach any rank or goal.
+	</div>
 </template>
 
 <script>
 export default {
-  layout: "game",
+	layout: "game",
 };
 </script>
