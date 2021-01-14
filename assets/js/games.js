c317e5058d07e0152618b5351ce286c9b5b80a99
Salim Djerbouh <caddydz4@gmail.com>
2021-01-14 12:48:45 +0100

    Added url of first service in game to game
    * Also specified currently disabled games
    
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,22 +1,27 @@
 export default [{
 		name: "LOL BOOSTING",
 		image: "/img/games/lol-boosting.png",
+		slug: "division-boosting",
 	},
 	{
 		name: "VALORANT BOOSTING",
 		image: "/img/games/valorant-boosting.png",
+		slug: "valorant-boosting",
 	},
 	{
 		name: "TFT Boosting",
 		image: "/img/games/tft-boosting.png",
+		disabled: true,
 	},
 	{
 		name: "Wild Rift Boosting",
 		image: "/img/games/wild-rift-boosting.png",
+		slug: "rank-boosting",
 	},
 	{
 		name: "RUNETERRA BOOSTING",
 		image: "/img/games/lor-boosting.png",
+		disabled: true,
 	},
 	// {
 	// 	name: "COACHING",
