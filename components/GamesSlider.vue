0b7d76aa199e04f172229ad72dfe9c1048ca0c4a
Salim Djerbouh <caddydz4@gmail.com>
2021-01-16 18:09:10 +0100

    Simpler yet responsive games cards design
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- components/GamesSlider.vue
+++ components/GamesSlider.vue
@@ -1,12 +1,9 @@
 <template>
-	<v-row class="cards">
-		<v-col class="card" v-for="(game, index) in games" :key="index">
-			<div class="card-img-hider">
-				<nuxt-link class="card-img-container flex-center" :to="game.slug" :class="{selectedGame: (index == currentGameIndex)}">
-					<div class="card-img-bg"></div>
-					<img class="card-img" :src="game.image" />
-				</nuxt-link>
-			</div>
+	<v-row>
+		<v-col class="card" v-for="(game, index) in games" :key="index" :class="{selectedGame: (index == currentGameIndex), disabled: game.disabled}">
+			<nuxt-link :to="game.slug || '/'" :event="game.disabled ? '' : 'click'" :class="{disabled: game.disabled}">
+				<img :src="game.image" width="100%" />
+			</nuxt-link>
 		</v-col>
 	</v-row>
 </template>
@@ -14,6 +11,7 @@
 <script>
 import games from "~/assets/js/games";
 import services from "~/assets/js/services";
+
 export default {
 	data: () => ({
 		currentGameIndex: 0,
@@ -34,67 +32,30 @@ export default {
 </script>
 
 <style scoped>
-.flex-center {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-}
-
-.cards {
-	display: grid;
-	grid-template-columns: repeat(5, 1fr);
-}
-
 .card {
-	width: 100%;
-}
-
-.card-img-hider {
-	position: relative;
-	overflow: hidden;
-	transform: scale(1.2);
-	display: flex;
-	justify-content: center;
-	transition-duration: 0.3s;
-}
-
-.card-img-container {
-	position: relative;
-	width: 100%;
-	height: 320px;
-	transform: translateY(5em);
 	transition-duration: 0.3s;
 	transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
 	filter: grayscale(100%);
+	padding-bottom: 0;
+	line-height: 0;
 }
 
-.card-img-container:hover {
-	transform: translateY(1em);
+.card:hover {
+	transform: translateY(-30px);
 	cursor: pointer;
 	filter: grayscale(0%);
 }
 
+.disabled:hover {
+	transform: none;
+	cursor: not-allowed;
+}
+
 .selectedGame {
 	filter: grayscale(0%);
-	background: url(/img/backgrounds/active-game-bg.png) no-repeat bottom;
 }
 
-.card-img-bg {
-	position: absolute;
-	top: 0;
-	width: 58%;
-	height: 12em;
-	margin-top: 16px;
-	box-shadow: 0px 0px 22px 0px #28153f77;
-}
-
-.card-img-container:hover .card-img-bg {
-	box-shadow: 0px 0px 32px 0px #8746d4;
-}
-
-.card-img {
-	position: absolute;
-	top: 0;
-	width: 100%;
+.selectedGame:hover {
+	transform: none;
 }
 </style>
