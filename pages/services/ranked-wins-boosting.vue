2e183e388ea1a8a12c010ce335448fcb5f615cc2
Salim Djerbouh <caddydz4@gmail.com>
2021-01-19 18:47:22 +0100

    Pricing on ranked wins boosting
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/services/ranked-wins-boosting.vue
+++ pages/services/ranked-wins-boosting.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-row>
 		<v-col cols="12" sm="6" md="7">
-			<select-current-league :showPointsSelection="false" showServerSelection />
+			<select-current-league :showPointsSelection="false" showServerSelection :tiers="tiers" @divisionChanged="changePrice" />
 			<slider />
 		</v-col>
 		<v-col cols="6" md="5">
@@ -12,9 +12,12 @@
 </template>
 
 <script>
+import tiers from "~/assets/js/wins_prices";
+
 export default {
 	layout: "order",
 	data: () => ({
+		tiers: tiers,
 		options: [
 			{
 				checked: false,
@@ -66,8 +69,79 @@ export default {
 			},
 		],
 	}),
+	computed: {
+		tier() {
+			return _.find(this.tiers, ["id", this.$store.state.league.tier]);
+		},
+		division() {
+			return _.find(this.tier.divisions, [
+				"id",
+				this.$store.state.league.division,
+			]);
+		},
+	},
+	methods: {
+		changePrice() {
+			this.$store.commit("price/changePrice", this.division.price);
+		},
+		sendOrder(token) {
+			// Gather selected server
+			let server = this.$store.state.league.server;
+			// Get selected number of wins
+			let wins = this.$store.state.slider.amount;
+			// Gather game queue (solo/duo)
+			this.$store.commit(
+				"checkout/addOption",
+				this.$store.state.slider.queue
+			);
+			// Gather extra options
+			let options = this.$store.state.checkout.options;
+			// Gather discount code
+			let discountCode = this.$store.state.checkout.discountCode;
+			// Gather price
+			let price = this.$store.getters["price/price"];
+			// Construct the purchase string
+			// Purchase here is "2 wins in Gold IV"
+			let purchase = `${wins} wins in ${this.tier.name} ${this.division.name}`;
+			// Gather in-game-nickname (summoner name)
+			let nickname = this.$store.state.order.nickname;
+			// Gather selected booster
+			let booster = this.$store.state.order.booster;
+			// Gather Comment
+			let comment = this.$store.state.order.comment;
+			// Get all data from store and post them to DB
+			this.$axios
+				.post("orders", {
+					purchase,
+					service: "Ranked Win boosting",
+					server,
+					options,
+					price,
+					discountCode,
+					nickname,
+					booster,
+					comment,
+					token,
+				})
+				.then((response) => {
+					this.$notify(response.data.message, "success");
+					setTimeout(() => {
+						window.location = `${process.env.HOST_URL}/resources/orders/${response.data.order_id}`;
+					}, 4000);
+					// Actually just close the dialog, semantics ¯\_(ツ)_/¯
+					this.cancel();
+				})
+				.catch((errors) => {
+					this.$notify(errors.response.data.error, "error");
+				});
+		},
+	},
 	mounted() {
-		this.$store.commit("slider/changeAmount", 4);
+		setTimeout(() => {
+			this.$store.commit("slider/changeAmount", 4);
+			this.changePrice();
+			this.$root.$on("sendOrder", (token) => this.sendOrder(token));
+		}, 300);
 	},
 };
 </script>
