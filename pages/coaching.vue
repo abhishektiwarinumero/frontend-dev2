c326a732746d74d66808c976373258bf11b8711e
Salim Djerbouh <caddydz4@gmail.com>
2021-01-21 02:24:44 +0100

    Formatting
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/coaching.vue
+++ pages/coaching.vue
@@ -22,36 +22,25 @@
 				<v-col md="4">
 					<v-card class="mx-auto my-12">
 						<v-img height="250" src="/img/coaching/professor-ryze.png"></v-img>
-
 						<v-card-title>Cafe Badilico</v-card-title>
-
 						<v-card-text>
 							<v-row align="center" class="mx-0">
 								<v-rating :value="4.5" color="amber" dense half-increments readonly size="14"></v-rating>
-
 								<div class="grey--text ml-4">4.5 (413)</div>
 							</v-row>
-
 							<div class="my-4 subtitle-1">$ • Italian, Cafe</div>
-
 							<div>
 								Small plates, salads & sandwiches - an intimate setting with 12
 								indoor seats plus patio seating.
 							</div>
 						</v-card-text>
-
 						<v-divider class="mx-4"></v-divider>
-
 						<v-card-title>Tonight's availability</v-card-title>
-
 						<v-card-text>
 							<v-chip-group active-class="deep-purple accent-4 white--text" column>
 								<v-chip>5:30PM</v-chip>
-
 								<v-chip>7:30PM</v-chip>
-
 								<v-chip>8:00PM</v-chip>
-
 								<v-chip>9:00PM</v-chip>
 							</v-chip-group>
 						</v-card-text>
