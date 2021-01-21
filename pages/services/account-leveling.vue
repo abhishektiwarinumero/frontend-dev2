8d31c64736f267491a1aacb40784570da12fa218
Salim Djerbouh <caddydz4@gmail.com>
2021-01-21 18:04:30 +0100

    Account leveling pricing
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/services/account-leveling.vue
+++ pages/services/account-leveling.vue
@@ -6,20 +6,20 @@
 					<!-- First we need to divide a row into two columns -->
 					<v-row>
 						<v-col>
-							<v-btn color="primary" elevation="24" icon outlined x-large @click="currentLevel--">
+							<v-btn color="primary" elevation="24" icon outlined x-large @click="decreaseCurrent">
 								<v-icon>mdi-minus</v-icon>
 							</v-btn>
 							{{ currentLevel }}
-							<v-btn color="primary" elevation="24" icon outlined x-large @click="currentLevel++">
+							<v-btn color="primary" elevation="24" icon outlined x-large @click="increaseCurrent">
 								<v-icon>mdi-plus</v-icon>
 							</v-btn>
 						</v-col>
 						<v-col>
-							<v-btn color="primary" elevation="24" icon outlined x-large @click="desiredLevel--">
+							<v-btn color="primary" elevation="24" icon outlined x-large @click="decreaseDesired">
 								<v-icon>mdi-minus</v-icon>
 							</v-btn>
 							{{ desiredLevel }}
-							<v-btn color="primary" elevation="24" icon outlined x-large @click="desiredLevel++">
+							<v-btn color="primary" elevation="24" icon outlined x-large @click="increaseDesired">
 								<v-icon>mdi-plus</v-icon>
 							</v-btn>
 						</v-col>
@@ -35,9 +35,12 @@
 </template>
 
 <script>
+import levels from "~/assets/js/levels";
+
 export default {
 	layout: "order",
 	data: () => ({
+		levels: levels,
 		options: [
 			{
 				checked: false,
@@ -80,8 +83,52 @@ export default {
 					"If you will provide XP boost for every game, select this option for a cheaper price.",
 			},
 		],
-		currentLevel: 30,
-		desiredLevel: 50,
+		currentLevel: 2,
+		desiredLevel: 30,
 	}),
+	watch: {
+		currentLevel() {
+			this.changePrice();
+		},
+		desiredLevel() {
+			this.changePrice();
+		},
+	},
+	methods: {
+		increaseCurrent() {
+			if (this.currentLevel >= 29) return;
+			this.currentLevel++;
+		},
+		decreaseCurrent() {
+			if (this.currentLevel <= 2) return;
+			this.currentLevel--;
+		},
+		increaseDesired() {
+			if (this.desiredLevel >= 30) return;
+			this.desiredLevel++;
+		},
+		decreaseDesired() {
+			if (this.desiredLevel <= 3) return;
+			this.desiredLevel--;
+		},
+		changePrice() {
+			let filteredLevels = _.filter(this.levels, (level) =>
+				// This is a whereIn replacement
+				_.range(this.desiredLevel, this.currentLevel).includes(
+					level.level
+				)
+			);
+			// Sum up their prices using JS reduce
+			let total = filteredLevels.reduce((sum, level) => {
+				return sum + level.price;
+			}, 0);
+			// Get the current level and desired level
+			// Accumulate the prices based on the range from lodash
+			this.$store.commit("price/changePrice", total);
+		},
+	},
+	mounted() {
+		this.changePrice();
+	},
 };
 </script>
