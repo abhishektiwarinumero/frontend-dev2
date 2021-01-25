e930f3d82f88cd9bc8f1031c11e849d3d3e37808
Salim Djerbouh <caddydz4@gmail.com>
2021-01-25 14:14:18 +0100

    Work In Progress
    This is a trash commit to save changes
    
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/services/wild-rift-boosting.vue
+++ pages/services/wild-rift-boosting.vue
@@ -1,8 +1,8 @@
 <template>
 	<v-row>
 		<v-col cols="12" sm="6" md="7">
-			<current @divisionChanged="changePrice" @lpchanged="changePrice" @mmrchanged="changePrice" marks :tiers="tiers" />
-			<desired @divisionChanged="changePrice" />
+			<current title="Current League" description="Please select your Current Rank and Division" @divisionChanged="changePrice" @lpchanged="changePrice" @mmrchanged="changePrice" marks :tiers="tiers" />
+			<desired @divisionChanged="changePrice" :tiers="tiers" />
 		</v-col>
 		<v-col cols="6" md="5">
 			<checkout :options="options" />
@@ -38,22 +38,20 @@ export default {
 			let filteredPrices = _.filter(allPrices, (price) =>
 				_.range(
 					this.$store.state.desired.division,
-					this.$store.state.league.division
+					this.$store.state.current.division
 				).includes(price[0])
 			);
 			// Sum up their prices using JS reduce
 			let total = filteredPrices.reduce((sum, price) => {
 				return sum + price[1];
 			}, 0);
-			// Get the LP from the VueX store
-			let lp = this.$store.state.league.lp;
 			// Load price of (get it from currently selected desired division)
 			this.$store.commit("price/changePrice", total);
 		},
 	},
 	mounted() {
 		// Set Silver Division 1 as the default current
-		this.$store.commit("league/changeDivision", 11);
+		this.$store.commit("current/changeDivision", 11);
 		this.$store.commit("desired/changeDivision", 16);
 		this.changePrice();
 	},
