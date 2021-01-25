067d9fea813ebc4bae2b703717b026e699926378
Salim Djerbouh <caddydz4@gmail.com>
2021-01-25 13:46:52 +0100

    Extracted LOL Clash Boosting tiers
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/services/clash-boosting.vue
+++ pages/services/clash-boosting.vue
@@ -19,29 +19,13 @@
 
 <script>
 import servers from "~/assets/js/servers";
+import tiers from "~/assets/js/clash-boosting/tiers";
 
 export default {
 	layout: "order",
 	data: () => ({
 		tier: {},
-		tiers: [
-			{
-				label: "Tier I",
-				price: 12,
-			},
-			{
-				label: "Tier II",
-				price: 9,
-			},
-			{
-				label: "Tier III",
-				price: 7.5,
-			},
-			{
-				label: "Tier IV",
-				price: 6.2,
-			},
-		],
+		tiers: tiers,
 		servers: servers,
 		server: "EU-West",
 	}),
