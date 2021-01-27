1e06bb7c86720028b6d9bf446cdec9416cf8a65a
Salim Djerbouh <caddydz4@gmail.com>
2021-01-27 09:57:43 +0100

    Fixed DuoQueue Boosting price
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/services/duoqueue-boosting.vue
+++ pages/services/duoqueue-boosting.vue
@@ -1,8 +1,8 @@
 <template>
 	<v-row>
 		<v-col cols="12" sm="6" md="7">
-			<current title="Current League" description="Please select your Current Rank and Division" mmrs @divisionChanged="changePrice" @lpchanged="changePrice" @mmrchanged="changePrice" showPointsSelection :tiers="tiers" />
-			<desired @divisionChanged="changePrice" :tiers="tiers" />
+			<current title="Current League" description="Please select your Current Rank and Division" :tiers="tiers" mmrs suffix="Current LP" showPointsSelection lpTooltip="Your current Rank Rating amount." @divisionChanged="changePrice" @lpchanged="changePrice" @mmrchanged="changePrice" />
+			<desired title="Desired League" description="Please select your Desired Rank and Division" :tiers="tiers" @divisionChanged="changePrice" />
 		</v-col>
 		<v-col cols="6" md="5">
 			<checkout :options="options" />
@@ -21,74 +21,90 @@ export default {
 		tiers: tiers,
 		options: options,
 	}),
+	computed: {
+		currentTier() {
+			return this.$store.state.current.tier;
+		},
+		currentDivision() {
+			return this.$store.state.current.division;
+		},
+		desiredTier() {
+			return this.$store.state.desired.tier;
+		},
+		desiredDivision() {
+			return this.$store.state.desired.division;
+		},
+		mmr() {
+			return this.$store.state.current.mmr;
+		},
+		lp() {
+			return this.$store.state.current.lp;
+		},
+	},
 	methods: {
 		changePrice() {
-			let allPrices = _.flatten(
+			// Flatten and merge all divisions by id and price
+			// Add the Master+ tier price as the last one
+			let all_divisions_prices = _.flatten(
 				_.map(this.tiers, (tier) => {
 					if (tier.divisions) {
-						return _.map(tier.divisions, (division) => [
-							division.id,
-							division.price,
-						]);
+						return _.map(tier.divisions, (division) => ({
+							id: division.id,
+							price: division.price,
+							mmr: _.find(tier.mmrs, ["range", this.mmr.range])
+								.price,
+						}));
 					} else {
-						return tier.price;
+						return { id: 25, price: tier.price };
 					}
 				})
 			);
-			let filteredPrices = _.filter(allPrices, (price) =>
-				// This is a whereIn replacement
-				_.range(
-					this.$store.state.desired.division,
-					this.$store.state.current.division
-				).includes(price[0])
+			let fromId;
+			if (this.currentTier.divisions) {
+				fromId = this.currentDivision.id;
+			} else {
+				fromId = 25;
+			}
+			let toId;
+			if (this.desiredTier.divisions) {
+				toId = this.desiredDivision.id;
+			} else {
+				toId = 25;
+			}
+			let divisions_in_between = _.filter(
+				all_divisions_prices,
+				(division) =>
+					// This is a whereIn replacement
+					this.$range(fromId, toId).includes(division.id)
 			);
 			// Sum up their prices using JS reduce
-			let total = filteredPrices.reduce((sum, price) => {
-				return sum + price[1];
+			let total = divisions_in_between.reduce((sum, division) => {
+				return sum + division.price + division.mmr;
 			}, 0);
-			// Get the MMR from the VueX store
-			// We need to get its price from the currently selected division
-			let tier = this.$store.state.current.tier;
-
-			let filtered_mmrs = _.filter(tier.mmrs, (mmr) =>
-				_.range(this.$store.state.current.mmr.id, 1).includes(mmr.id)
-			);
-
-			let mmr_total = filtered_mmrs.reduce(
-				(sum, mmr) => sum + mmr.price,
-				0
-			);
-
-			total = total * 1.5;
-
-			// Load price of (get it from currently selected desired division)
-			this.$store.commit(
-				"price/changePrice",
-				total + mmr_total - tier.lp
-			);
+			if (this.lp) {
+				// Load price of (get it from currently selected desired division)
+				this.$store.commit(
+					"price/changePrice",
+					(total - this.lp.price) * 1.5
+				);
+			}
 		},
 		sendOrder(token) {
-			// Gather current tier and division
-			let currentTier = this.$store.state.current.tier;
-			let currentDivision = this.$store.state.current.division;
-			// Gather desired tier and division
-			let desiredTier = this.$store.state.desired.tier;
-			let desiredDivision = this.$store.state.desired.division;
 			// Gather LP
 			this.$store.commit(
 				"checkout/addOption",
 				// We shouldn't send the value of the LP, but the rank name and suffix it with LP
-				this.$store.state.current.lp + " LP"
+				this.lp.label + " LP"
 			);
 			// Gather MMR
 			this.$store.commit(
 				"checkout/addOption",
 				// Same thing goes for MMR as stated above
-				`+${this.$store.state.current.mmr.range} LP`
+				`+${this.mmr.range} LP`
 			);
 			// Construct the purchase string
 			// Purchase here is "current (tier & division) to desired (tier & division)"
-			let purchase = `${currentTier.name} ${currentDivision.name} to ${desiredTier.name} ${desiredDivision.name}`;
+			let purchase = `${this.currentTier.name} ${this.currentDivision.name} to ${this.desiredTier.name} ${this.desiredDivision.name}`;
 			// Gather game queue (solo/duo)
 			this.$store.commit(
 				"checkout/addOption",
@@ -99,7 +115,7 @@ export default {
 				.post("orders", {
 					purchase,
 					service: "DuoQueue Division Boosting",
-					server: this.$store.state.current.server,
+					server: this.$store.state.desired.server,
 					options: this.$store.state.checkout.options,
 					price: this.$store.getters["price/price"],
 					discountCode: this.$store.state.checkout.discountCode,
@@ -122,7 +138,6 @@ export default {
 		},
 	},
 	mounted() {
-		this.changePrice();
 		this.$root.$on("sendOrder", (token) => this.sendOrder(token));
 	},
 };
