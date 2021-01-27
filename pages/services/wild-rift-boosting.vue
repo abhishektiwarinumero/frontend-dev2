0864736cdcd4d4f39e45fc6647e86988b68e0490
Salim Djerbouh <caddydz4@gmail.com>
2021-01-27 17:04:33 +0100

    Wildrift ranked wins boosting initial pricing
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/services/wild-rift-boosting.vue
+++ pages/services/wild-rift-boosting.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-row>
 		<v-col cols="12" sm="6" md="7">
-			<current title="Current League" description="Please select your Current Rank and Division" @divisionChanged="changePrice" @lpchanged="changePrice" @mmrchanged="changePrice" marks :tiers="tiers" />
+			<current title="Current League" description="Please select your Current Rank and Division" @divisionChanged="changePrice" @mmrchanged="changePrice" marks :tiers="tiers" />
 			<desired @divisionChanged="changePrice" :tiers="tiers" />
 		</v-col>
 		<v-col cols="6" md="5">
@@ -21,39 +21,92 @@ export default {
 		tiers: tiers,
 		options: options,
 	}),
+	computed: {
+		currentTier() {
+			return this.$store.state.current.tier;
+		},
+		currentDivision() {
+			return this.$store.state.current.division;
+		},
+		desiredTier() {
+			return this.$store.state.desired.tier;
+		},
+		desiredDivision() {
+			return this.$store.state.desired.division;
+		},
+	},
 	methods: {
 		changePrice() {
-			let allPrices = _.flatten(
+			// Flatten and merge all divisions by id and price
+			// Add the Master+ tier price as the last one
+			let all_divisions_prices = _.flatten(
 				_.map(this.tiers, (tier) => {
-					if (tier.divisions) {
-						return _.map(tier.divisions, (division) => [
-							division.id,
-							division.price,
-						]);
-					} else {
-						return tier.price;
-					}
+					return _.map(tier.divisions, (division) => ({
+						id: division.id,
+						price: division.price,
+					}));
 				})
 			);
-			let filteredPrices = _.filter(allPrices, (price) =>
-				_.range(
-					this.$store.state.desired.division,
-					this.$store.state.current.division
-				).includes(price[0])
+			let divisions_in_between = _.filter(
+				all_divisions_prices,
+				(division) =>
+					// This is a whereIn replacement
+					this.$range(
+						this.currentDivision.id,
+						this.desiredDivision.id
+					).includes(division.id)
 			);
 			// Sum up their prices using JS reduce
-			let total = filteredPrices.reduce((sum, price) => {
-				return sum + price[1];
+			let total = divisions_in_between.reduce((sum, division) => {
+				return sum + division.price;
 			}, 0);
 			// Load price of (get it from currently selected desired division)
 			this.$store.commit("price/changePrice", total);
 		},
+		sendOrder(token) {
+			// Gather LP
+			this.$store.commit(
+				"checkout/addOption",
+				// We shouldn't send the value of the LP, but the rank name and suffix it with LP
+				this.lp.label + " LP"
+			);
+			// Construct the purchase string
+			// Purchase here is "current (tier & division) to desired (tier & division)"
+			let purchase = `${this.currentTier.name} ${this.currentDivision.name} to ${this.desiredTier.name} ${this.desiredDivision.name}`;
+			// Gather game queue (solo/duo)
+			this.$store.commit(
+				"checkout/addOption",
+				this.$store.state.desired.queue
+			);
+			// Get all data from store and post them to DB
+			this.$axios
+				.post("orders", {
+					purchase,
+					service: "Division Boosting",
+					server: this.$store.state.desired.server,
+					options: this.$store.state.checkout.options,
+					price: this.$store.getters["price/price"],
+					discountCode: this.$store.state.checkout.discountCode,
+					nickname: this.$store.state.order.nickname,
+					booster: this.$store.state.order.booster,
+					comment: this.$store.state.order.comment,
+					token,
+				})
+				.then((response) => {
+					this.$notify(response.data.message, "success");
+					setTimeout(() => {
+						window.location = `${process.env.HOST_URL}/resources/orders/${response.data.order_id}`;
+					}, 4000);
+					// Actually just close the dialog, semantics ¯\_(ツ)_/¯
+					this.cancel();
+				})
+				.catch((errors) => {
+					this.$notify(errors.response.data.error, "error");
+				});
+		},
 	},
 	mounted() {
-		// Set Silver Division 1 as the default current
-		this.$store.commit("current/changeDivision", 11);
-		this.$store.commit("desired/changeDivision", 16);
-		this.changePrice();
+		this.$root.$on("sendOrder", (token) => this.sendOrder(token));
 	},
 };
 </script>
