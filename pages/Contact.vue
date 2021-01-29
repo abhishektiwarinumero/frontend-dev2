d54dc2e654999d873da795eb8ff4cf4e0cf89b4d
Salim Djerbouh <caddydz4@gmail.com>
2021-01-29 22:39:25 +0100

    Contact form application redirect
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- pages/Contact.vue
+++ pages/Contact.vue
@@ -42,9 +42,75 @@
 					</v-card-text>
 				</v-card>
 			</v-row>
+			<v-row>
+				<v-col>
+					<v-form ref="form" v-model="valid">
+						<v-select v-model="category" name="Category" label="Category" :rules="[(v) => !!v || 'Category is required']" :items="categories"></v-select>
+						<v-text-field name="name" label="Name" :rules="nameRules"></v-text-field>
+						<v-text-field name="email" label="E-mail address" :rules="[(v) => /.+@.+\..+/.test(v) || 'E-mail must be valid']"></v-text-field>
+						<v-textarea outlined name="message" placeholder="Your Message"></v-textarea>
+						<v-btn :disabled="!valid" color="primary" class="mr-4" @click="submit">Send Message</v-btn>
+					</v-form>
+				</v-col>
+			</v-row>
 		</v-container>
 	</div>
 </template>
+
+<script>
+export default {
+	data: () => ({
+		categories: [
+			"General Question",
+			"Order Issue",
+			"Application",
+			"Bug Report",
+			"Other",
+		],
+		category: "General Question",
+		valid: false,
+		nameRules: [
+			(v) =>
+				(v && v.length <= 10) || "Name must be less than 10 characters",
+		],
+	}),
+	watch: {
+		category(value) {
+			if (value == "Application") {
+				this.$router.push("/apply");
+			}
+		},
+	},
+	methods: {
+		submit() {
+			this.validate();
+			if (this.valid) {
+				const formData = new FormData(this.$refs.form.$el); // reference to form element
+				const data = {}; // need to convert it before using not with XMLHttpRequest
+				for (let [key, val] of formData.entries()) {
+					Object.assign(data, { [key]: val });
+				}
+				this.$axios
+					.post("api/contact", data)
+					.then((response) => {
+						this.$notify(response.data.message, "success");
+						this.$refs.form.reset();
+					})
+					.catch((errors) => {
+						this.$notify(
+							errors.response.data.message,
+							"error",
+							errors.response.data.errors
+						);
+					});
+			}
+		},
+		validate() {
+			this.$refs.form.validate();
+		},
+	},
+};
+</script>
 
 <style scoped>
 .contact-page {
